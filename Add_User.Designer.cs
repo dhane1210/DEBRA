9c9b7c4a74f720b982bc0e885da54393f46d086f
Dhanesha Manorathne <dhanesha1210@gmail.com>
2024-06-27 12:20:57 +0530

    Add project files.

---
+++ Add_User.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DEBRA
+{
+    partial class Add_User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_new = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_un = new System.Windows.Forms.TextBox();
+            this.txt_pw = new System.Windows.Forms.TextBox();
+            this.cmb_role = new System.Windows.Forms.ComboBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.grv_user = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grv_user)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_new
+            // 
+            this.btn_new.Location = new System.Drawing.Point(47, 197);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(160, 23);
+            this.btn_new.TabIndex = 0;
+            this.btn_new.Text = "New";
+            this.btn_new.UseVisualStyleBackColor = true;
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(47, 241);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(160, 23);
+            this.btn_edit.TabIndex = 1;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(47, 285);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(160, 23);
+            this.btn_save.TabIndex = 2;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(47, 330);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(160, 23);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "Delete";
+            this.btn_del.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Role";
+            // 
+            // txt_un
+            // 
+            this.txt_un.Location = new System.Drawing.Point(103, 41);
+            this.txt_un.Name = "txt_un";
+            this.txt_un.Size = new System.Drawing.Size(121, 22);
+            this.txt_un.TabIndex = 8;
+            // 
+            // txt_pw
+            // 
+            this.txt_pw.Location = new System.Drawing.Point(103, 86);
+            this.txt_pw.Name = "txt_pw";
+            this.txt_pw.Size = new System.Drawing.Size(121, 22);
+            this.txt_pw.TabIndex = 9;
+            // 
+            // cmb_role
+            // 
+            this.cmb_role.FormattingEnabled = true;
+            this.cmb_role.Location = new System.Drawing.Point(103, 132);
+            this.cmb_role.Name = "cmb_role";
+            this.cmb_role.Size = new System.Drawing.Size(121, 24);
+            this.cmb_role.TabIndex = 10;
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(47, 374);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(160, 23);
+            this.btn_Cancel.TabIndex = 4;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // grv_user
+            // 
+            this.grv_user.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grv_user.Location = new System.Drawing.Point(255, 41);
+            this.grv_user.Name = "grv_user";
+            this.grv_user.RowHeadersWidth = 51;
+            this.grv_user.RowTemplate.Height = 24;
+            this.grv_user.Size = new System.Drawing.Size(383, 332);
+            this.grv_user.TabIndex = 11;
+            // 
+            // Add_User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 451);
+            this.Controls.Add(this.grv_user);
+            this.Controls.Add(this.cmb_role);
+            this.Controls.Add(this.txt_pw);
+            this.Controls.Add(this.txt_un);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_new);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(681, 498);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(681, 498);
+            this.Name = "Add_User";
+            this.Text = "frm_Add_User";
+            ((System.ComponentModel.ISupportInitialize)(this.grv_user)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_un;
+        private System.Windows.Forms.TextBox txt_pw;
+        private System.Windows.Forms.ComboBox cmb_role;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.DataGridView grv_user;
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DEBRA
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_un = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_un
+            // 
+            this.txt_un.Location = new System.Drawing.Point(293, 49);
+            this.txt_un.Name = "txt_un";
+            this.txt_un.Size = new System.Drawing.Size(122, 22);
+            this.txt_un.TabIndex = 0;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(293, 103);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(122, 22);
+            this.txt_pass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(181, 176);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(340, 176);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 5;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 296);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_un);
+            this.Name = "frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_un;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
+
